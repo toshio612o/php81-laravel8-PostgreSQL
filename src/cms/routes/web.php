57afdff6dbb1cb2c8c0ec6f0a4885ed38ba8f38d
toshio612o <toshio.engineer0612@gmail.com>
2022-01-02 17:21:07 +0900

    ファイルコピー

--- src/cms/routes/web.php
+++ src/cms/routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Book;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,20 +11,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Book;
+use Illuminate\Http\Request;
+
+/**
+* 本の一覧表示(books.blade.php)
+*/
 Route::get('/', function () {
     return view('welcome');
 });
 
 /**
- * 本を追加
- */
-Route::post('/books', function (Request $request){
-
+* 本を追加 
+*/
+Route::post('/books', function (Request $request) {
+    //
 });
 
 /**
- * 本を削除
- */
-Route::delete('/book/{book}', function (Book $book){
-
+* 本を削除 
+*/
+Route::delete('/book/{book}', function (Book $book) {
+    //
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
