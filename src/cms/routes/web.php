42d511d9af307d8d9729e07ac3519bdb5fb19720
toshio612o <toshio.engineer0612@gmail.com>
2022-01-03 17:01:04 +0900

    更新処理とバリデーション修正

--- src/cms/routes/web.php
+++ src/cms/routes/web.php
@@ -18,33 +18,71 @@ use Illuminate\Support\Facades\Route;
 /**
 * 本の一覧表示(books.blade.php)
 */
+
 Route::get('/', function () {
-    $books = Book::orderBy('created_at','asc')->get();
-    return view('books',['books' => $books]);
+    $books = Book::orderBy('created_at', 'asc')->get();
+    return view('books', [
+        'books' => $books
+    ]);
+    //return view('books',compact('books')); //も同じ意味
 });
 
 /**
-* 本を追加
-*/
+ * 本を登録
+ */
 Route::post('/books', function (Request $request) {
-    // バリデーション
-    $validator = Validator::make($request->all(),[
-        'item_name' => 'required|max:255',
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required | min:1 | max:3',
+        'item_amount' => 'required | max:6',
+        'published'   => 'required',
     ]);
-
-    // バリデーションエラー
+    //バリデーション:エラー
     if ($validator->fails()) {
         return redirect('/')
-        ->withInput()
-        ->withErrors($validator);
+            ->withInput()
+            ->withErrors($validator);
+    }
+    // Eloquentモデル（登録処理）
+    $books = new Book;
+    $books->item_name =    $request->item_name;
+    $books->item_number =  $request->item_number;
+    $books->item_amount =  $request->item_amount;
+    $books->published =    $request->published;
+    $books->save();
+    return redirect('/');
+});
+
+//更新画面
+Route::post('/booksedit/{books}', function (Book $books) {
+    //{books}id 値を取得 => Book $books id 値の1レコード取得
+    return view('booksedit', ['book' => $books]);
+});
+
+//更新処理
+Route::post('/books/update', function (Request $request) {
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'id' => 'required',
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required|min:1|max:3',
+        'item_amount' => 'required|max:6',
+        'published' => 'required',
+    ]);
+    //バリデーション:エラー
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
     }
 
-    // Eloquent モデル（登録処理）
-    $books = new Book;
-    $books->item_name = $request->item_name;
-    $books->item_number = '1';
-    $books->item_amount = '1000';
-    $books->published = '2021-01-07 00:00:00';
+    //データ更新
+    $books = Book::find($request->id);
+    $books->item_name   = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published   = $request->published;
     $books->save();
     return redirect('/');
 });
