6f894921626cf39d7ba853bfaf84b1970523ceaf
toshio612o <toshio.engineer0612@gmail.com>
2022-01-03 18:43:12 +0900

    ページネーション

--- src/cms/app/Http/Controllers/BooksController.php
+++ src/cms/app/Http/Controllers/BooksController.php
@@ -10,7 +10,7 @@ use Auth;
 class BooksController extends Controller
 {
     public function index(Request $request){
-        $books = Book::orderBy('created_at', 'asc')->get();
+        $books = Book::orderBy('created_at', 'asc')->paginate(3);
         return view('books', [
             'books' => $books
         ]);
--- src/cms/resources/views/books.blade.php
+++ src/cms/resources/views/books.blade.php
@@ -94,5 +94,10 @@
         </table>
     </div>
 </div>
+<div class="row">
+    <div class="col-md-4 offset-md-4">
+        {{ $books->links() }}
+    </div>
+</div>
 @endif
 @endsection
