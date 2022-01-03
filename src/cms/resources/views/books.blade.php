64a7c303a6651897f8924ee3286229f1832542dd
toshio612o <toshio.engineer0612@gmail.com>
2022-01-03 16:01:23 +0900

    リストの表示

--- src/cms/resources/views/books.blade.php
+++ src/cms/resources/views/books.blade.php
@@ -32,4 +32,30 @@
         </div>
     </form>
 </div>
+<!-- 現在の本 -->
+@if (count($books) > 0)
+<div class="card-body">
+    <div class="card-body">
+        <table class="table table-striped task-table">
+            <thead>
+                <th>本一覧</th>
+                <td>&nbsp;</td>
+            </thead>
+            <!-- テーブル本体 -->
+            <tbody>
+                @foreach ($books as $book)
+                <tr>
+                    <!-- 本タイトル -->
+                    <td class="table-text">
+                        <div> {{$book->item_name}} </div>
+                    </td>
+                    <!-- 本：削除ボタン -->
+                    <td></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endif
 @endsection
